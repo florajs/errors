f8995818a5ccffe6bcc97af56202866bec129e9f
Nico Kaiser <nico.kaiser@boerse-go.de>
2016-12-06 14:40:57 +0100

    Update to ES6 syntax

--- index.js
+++ index.js
@@ -1,110 +1,78 @@
 'use strict';
 
-var util = require('util');
+const has = require('has');
 
-/**
- * Base class for Flora errors.
- *
- * @extends Error
- * @constructor
- * @param {string} message
- */
-function FloraError(message) {
-    Error.captureStackTrace(this, this.constructor);
-    this.name = this.constructor.name;
+class FloraError extends Error {
+    constructor(message, ...args) {
+        super(message, ...args);
 
-    /**
-     * @type {string}
-     * @name FloraError#message
-     * @readonly
-     */
-    this.message = message;
+        Error.captureStackTrace(this, this.constructor);
+        this.name = this.constructor.name;
 
-    /**
-     * @type {number}
-     * @name FloraError#httpStatusCode
-     * @readonly
-     */
-    this.httpStatusCode = 500; // HTTP status for "other errors"
+        /**
+         * @type {string}
+         * @name FloraError#message
+         * @readonly
+         */
+        this.message = message;
+
+        /**
+         * @type {number}
+         * @name FloraError#httpStatusCode
+         * @readonly
+         */
+        this.httpStatusCode = 500; // HTTP status for "other errors"
+    }
 }
-util.inherits(FloraError, Error);
 
-/**
- * @extends FloraError
- * @constructor
- * @param {string} message
- */
-function RequestError(message) {
-    FloraError.call(this, message);
-    this.httpStatusCode = 400;
+class RequestError extends FloraError {
+    constructor(...args) {
+        super(...args);
+        this.httpStatusCode = 400;
+    }
 }
-util.inherits(RequestError, FloraError);
 
-/**
- * @extends FloraError
- * @constructor
- * @param {string} message
- */
-function AuthenticationError(message) {
-    FloraError.call(this, message);
-    this.httpStatusCode = 401;
+class AuthenticationError extends FloraError {
+    constructor(...args) {
+        super(...args);
+        this.httpStatusCode = 401;
+    }
 }
-util.inherits(AuthenticationError, FloraError);
 
-/**
- * @extends FloraError
- * @constructor
- * @param {string} message
- */
-function AuthorizationError(message) {
-    FloraError.call(this, message);
-    this.httpStatusCode = 403;
+class AuthorizationError extends FloraError {
+    constructor(...args) {
+        super(...args);
+        this.httpStatusCode = 403;
+    }
 }
-util.inherits(AuthorizationError, FloraError);
 
-/**
- * @extends FloraError
- * @constructor
- * @param {string} message
- */
-function NotFoundError(message) {
-    FloraError.call(this, message);
-    this.httpStatusCode = 404;
+class NotFoundError extends FloraError {
+    constructor(...args) {
+        super(...args);
+        this.httpStatusCode = 404;
+    }
 }
-util.inherits(NotFoundError, FloraError);
 
-/**
- * @extends FloraError
- * @constructor
- * @param {string} message
- */
-function ImplementationError(message) {
-    FloraError.call(this, message);
-    this.httpStatusCode = 500;
+class ImplementationError extends FloraError {
+    constructor(...args) {
+        super(...args);
+        this.httpStatusCode = 500;
+    }
 }
-util.inherits(ImplementationError, FloraError);
 
-/**
- * @extends FloraError
- * @constructor
- * @param {string} message
- */
-function DataError(message) {
-    FloraError.call(this, message);
-    this.httpStatusCode = 500;
+class DataError extends FloraError {
+    constructor(...args) {
+        super(...args);
+        this.httpStatusCode = 500;
+    }
 }
-util.inherits(DataError, FloraError);
 
-/**
- * @extends FloraError
- * @constructor
- * @param {string} message
- */
-function ConnectionError(message) {
-    FloraError.call(this, message);
-    this.httpStatusCode = 503;
+class ConnectionError extends FloraError {
+    constructor(...args) {
+        super(...args);
+        this.httpStatusCode = 503;
+    }
 }
-util.inherits(ConnectionError, FloraError);
 
 /**
  * Converts an error object to a stringifyable object format for use
@@ -114,7 +82,7 @@ util.inherits(ConnectionError, FloraError);
  * @param {Object=} options
  */
 function format(err, options) {
-    var error = {message: 'Internal Server Error'};
+    const error = { message: 'Internal Server Error' };
 
     options = options || {};
 
@@ -127,9 +95,10 @@ function format(err, options) {
         error.stack = err.stack.split(/\r?\n/);
 
         if (err.info) {
-            for (var key in err.info) {
-                if (!error.hasOwnProperty(key) && err.info.hasOwnProperty(key)) error[key] = err.info[key];
-            }
+            Object.keys(err.info).forEach((key) => {
+                if (!has(error, key) &&
+                    has(err.info, key)) error[key] = err.info[key];
+            });
         }
     }
 
@@ -137,13 +106,13 @@ function format(err, options) {
 }
 
 module.exports = {
-    RequestError: RequestError,
-    AuthenticationError: AuthenticationError,
-    AuthorizationError: AuthorizationError,
-    NotFoundError: NotFoundError,
-    ImplementationError: ImplementationError,
-    DataError: DataError,
-    ConnectionError: ConnectionError,
-    FloraError: FloraError,
-    format: format
+    RequestError,
+    AuthenticationError,
+    AuthorizationError,
+    NotFoundError,
+    ImplementationError,
+    DataError,
+    ConnectionError,
+    FloraError,
+    format
 };
